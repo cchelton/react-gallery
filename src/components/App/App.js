22846531c9c4dcad3a3e956aacb61d0702c2ec14
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-11 15:47:12 -0500

    gets gallery from server on load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
 import "./App.css";
 import GalleryList from "../GalleryList/GalleryList";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    gallery: [],
+  };
+
+  //
+  //  EVENT HANDLERS
+  //
+
+  componentDidMount() {
+    this.getGallery();
+  }
+  //
+  //  SERVER API CALLS
+  //
+
+  getGallery() {
+    axios({
+      method: "GET",
+      url: "/gallery",
+    })
+      .then((response) => {
+        console.log(response.data);
+        this.setState(
+          {
+            gallery: [...response.data],
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      })
+      .catch((err) => {
+        console.log(`There was an error getting songs: ${err}`);
+      });
+  }
+
+  //
+  //  RENDER
+  //
+
   render() {
     return (
       <div className="App">

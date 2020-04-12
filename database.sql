b9cdda7975ffd6c6428bee27f24b1d94ef3bb8f9
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-12 11:12:26 -0500

    adds insert test case query

--- database.sql
+++ database.sql
@@ -7,3 +7,9 @@ CREATE TABLE "galleryItems" (
 	"description" varchar(1000),
 	"likes" integer NOT NULL
 );
+
+--insert item test case
+/*
+INSERT INTO "galleryItems" ("path", "description", "likes")
+VALUES ('images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park.', 0);
+*/

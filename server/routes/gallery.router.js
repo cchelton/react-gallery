7162d60162a2d5fb26c4c7942d644d3d7ddc3aa0
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-12 11:17:48 -0500

    app gets data from database

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,24 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+const pool = require("../modules/pool");
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
 // PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
+router.put("/like/:id", (req, res) => {
+  console.log(req.params);
+  const galleryId = req.params.id;
+  for (const galleryItem of galleryItems) {
+    if (galleryItem.id == galleryId) {
+      galleryItem.likes += 1;
     }
-    res.sendStatus(200);
+  }
+  res.sendStatus(200);
 }); // END PUT Route
 
 // GET Route
-router.get('/', (req, res) => {
-    res.send(galleryItems);
+router.get("/", (req, res) => {
+  const queryText = `SELECT * FROM "galleryItems" ORDER BY "id" ASC;`;
+  pool
+    .query(queryText)
+    .then((dbResponse) => {
+      console.table(dbResponse.rows);
+      res.send(dbResponse.rows);
+    })
+    .catch((err) => {
+      console.log(`GET ERROR: ${err}`);
+      res.sendStatus(500);
+    });
 }); // END GET Route
 
 module.exports = router;

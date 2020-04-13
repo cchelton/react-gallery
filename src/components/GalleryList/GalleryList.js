cd65ee8fecb514356936980ee65dc4da3cc01b94
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-12 20:28:42 -0500

    adds basic bounce animation to gallery.
    wish i could've made this better. Cant figure out why it only animates when the form card is affected.

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,20 +1,52 @@
-import React from "react";
-import GalleryItem from "../GalleryItem/GalleryItem";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import axios from "axios";
 import "./GalleryList.css";
+import GalleryItem from "../GalleryItem/GalleryItem";
 import AddItemForm from "../AddItemForm/AddItemForm";
 
+const spring = {
+  type: "spring",
+  damping: 20,
+  stiffness: 300,
+};
+
 function GalleryList(props) {
-  const elements = props.gallery.map((item, index) => (
-    <GalleryItem
-      imgURL={item.path}
-      description={item.description}
-      id={item.id}
-      likes={item.likes}
-      key={index}
-      putLikes={props.putLikes}
-      getGallery={props.getGallery}
-    />
-  ));
+  const [gallery, setGallery] = useState([]);
+
+  useEffect(() => {
+    getGallery();
+  }, []);
+
+  //
+  //  SERVER API CALLS
+  //
+
+  function getGallery() {
+    axios({
+      method: "GET",
+      url: "/gallery",
+    })
+      .then((response) => {
+        setGallery(response.data);
+      })
+      .catch((err) => {
+        console.log(`GET ERR: ${err}`);
+      });
+  }
+
+  function putLikes(id) {
+    axios({
+      method: "PUT",
+      url: `gallery/like/${id}`,
+    })
+      .then((response) => {
+        getGallery();
+      })
+      .catch((err) => {
+        console.log(`PUT ERR: ${err}`);
+      });
+  }
 
   //
   //  RENDER
@@ -22,8 +54,22 @@ function GalleryList(props) {
 
   return (
     <div className="GalleryList">
-      {elements}
-      <AddItemForm getGallery={props.getGallery} />
+      {gallery.map((item, index) => (
+        <motion.div layoutTransition={spring} key={index}>
+          <GalleryItem
+            imgURL={item.path}
+            description={item.description}
+            id={item.id}
+            likes={item.likes}
+            key={index}
+            putLikes={putLikes}
+            getGallery={getGallery}
+          />
+        </motion.div>
+      ))}
+      <motion.div layoutTransition={spring}>
+        <AddItemForm getGallery={getGallery} />
+      </motion.div>
     </div>
   );
 }
